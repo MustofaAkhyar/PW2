5b83f47a7e811d4358cbccd67c3cf77e08ed9002
MustofaAkhyar <165134256+MustofaAkhyar@users.noreply.github.com>
2024-10-17 14:36:23 +0700

    Update FakultasController.php

--- app/Http/Controllers/FakultasController.php
+++ app/Http/Controllers/FakultasController.php
@@ -51,7 +51,14 @@ class FakultasController extends Controller
      */
     public function show(Fakultas $fakultas)
     {
-        //
+     
+        $fakultas = Fakultas::find($fakultas);
+        $data['success'] = true;
+        $data['message'] = "Detail data fakultas";
+        $data['result'] = $fakultas;
+        return response()->json($data, Response::HTTP_OK);
+
+        
     }
 
     /**
