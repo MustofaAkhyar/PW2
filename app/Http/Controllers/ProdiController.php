6be9203bd9ff7af8625bd8a411b1cbe84a3b4430
MustofaAkhyar <165134256+MustofaAkhyar@users.noreply.github.com>
2024-10-22 14:57:07 +0700

    Update ProdiController.php

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -46,20 +46,7 @@ class ProdiController extends Controller
         }
     }
 
-public function show($prodi)
-    {
-        $prodi = Prodi::with('fakultas')->find($prodi);
-        if ($prodi) {
-            $data['success'] = true;
-            $data['message'] = "Data prodi berhasil ditemukan";
-            $data['result'] = $prodi;
-            return response()->json($data, Response::HTTP_OK);
-        } else {
-            $data['success'] = false;
-            $data['message'] = "Data prodi tidak ditemukan";
-            return response()->json($data, Response::HTTP_NOT_FOUND);
-        }
-    }
+
 
 
     
