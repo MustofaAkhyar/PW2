c84f0fcbf4300491c9888da61a7b06465418f878
MustofaAkhyar <165134256+MustofaAkhyar@users.noreply.github.com>
2024-10-22 14:37:48 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::post('/mahasiswa',[MahasiswaController::class, 'store']);
 Route::patch('/fakultas/{fakultas}', [FakultasController::class,'update']);
 Route::patch('/prodi/{prodi}', [ProdiController::class,'update']);
 Route::delete('/fakultas/{fakultas}',[FakultasController::class, 'destroy']);
+Route::delete('/prodi/{prodi}', [ProdiController::class, 'destroy']);
